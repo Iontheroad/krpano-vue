9ce598aa0f38e6f2a68295fb5806e06564fd37e1
Peak <18240729508@163.com>
2023-07-24 17:48:56 +0800

    docs: 文档更新

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 module.exports = defineConfig({
+  publicPath: "./",
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
